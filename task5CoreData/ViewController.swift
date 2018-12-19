e824339e748bbc623dcec150c48370cf47b3ee22
Noura Khaled <nalmugarri@nouras-macbook-pro.local>
2018-12-19 08:16:16 +0300

    Initial Commit

---
+++ task5CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  task5CoreData
+//
+//  Created by Noura Khaled on 19/12/2018.
+//  Copyright © 2018 Noura Almugarri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
